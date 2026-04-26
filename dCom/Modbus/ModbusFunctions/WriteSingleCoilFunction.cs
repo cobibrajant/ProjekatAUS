3793acedb6027e8abcee58f267c913e878d75169
Lazar Mitrovic <lazarmitrvc211@gmail.com>
2026-04-26 21:54:09 +0200

    Omoguceno komandovanje kroz kontrolni prozor
    Implementirani write request response u WriteSingleCoilFunction i WriteSingleRegisterFunction. Omogucen upis pomocu ControlWindow

--- dCom/Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
+++ dCom/Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
@@ -2,7 +2,6 @@
 using Modbus.FunctionParameters;
 using System;
 using System.Collections.Generic;
-using System.Net;
 using System.Reflection;
 
 namespace Modbus.ModbusFunctions
@@ -24,15 +23,78 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters p = (ModbusWriteCommandParameters)CommandParameters;
+
+            byte[] request = new byte[12];
+
+            // MBAP
+            request[0] = (byte)(p.TransactionId >> 8);
+            request[1] = (byte)(p.TransactionId & 0xFF);
+            request[2] = (byte)(p.ProtocolId >> 8);
+            request[3] = (byte)(p.ProtocolId & 0xFF);
+            request[4] = (byte)(p.Length >> 8);
+            request[5] = (byte)(p.Length & 0xFF);
+            request[6] = p.UnitId;
+
+            // PDU (FC = 0x05)
+            request[7] = p.FunctionCode;
+            request[8] = (byte)(p.OutputAddress >> 8);
+            request[9] = (byte)(p.OutputAddress & 0xFF);
+
+            // Modbus coil write value: 0xFF00 = ON, 0x0000 = OFF
+            ushort coilValue = (p.Value == 0) ? (ushort)0x0000 : (ushort)0xFF00;
+            request[10] = (byte)(coilValue >> 8);
+            request[11] = (byte)(coilValue & 0xFF);
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters p = (ModbusWriteCommandParameters)CommandParameters;
+            var result = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            if (response == null || response.Length < 12)
+            {
+                throw new ArgumentException("Invalid Modbus response length.");
+            }
+
+            byte functionCode = response[7];
+            if ((functionCode & 0x80) != 0)
+            {
+                if (response.Length < 9)
+                {
+                    throw new ArgumentException("Invalid Modbus exception response length.");
+                }
+
+                HandeException(response[8]);
+            }
+
+            ushort address = (ushort)((response[8] << 8) | response[9]);
+            ushort echoedValue = (ushort)((response[10] << 8) | response[11]);
+
+            if (address != p.OutputAddress)
+            {
+                throw new ArgumentException("Write coil response address mismatch.");
+            }
+
+            ushort normalizedValue;
+            if (echoedValue == 0xFF00)
+            {
+                normalizedValue = 1;
+            }
+            else if (echoedValue == 0x0000)
+            {
+                normalizedValue = 0;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid write coil response value.");
+            }
+
+            result.Add(new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, address), normalizedValue);
+            return result;
         }
     }
 }
--- dCom/Modbus/ModbusFunctions/WriteSingleRegisterFunction.cs
+++ dCom/Modbus/ModbusFunctions/WriteSingleRegisterFunction.cs
@@ -2,7 +2,6 @@
 using Modbus.FunctionParameters;
 using System;
 using System.Collections.Generic;
-using System.Net;
 using System.Reflection;
 
 namespace Modbus.ModbusFunctions
@@ -25,14 +24,62 @@ namespace Modbus.ModbusFunctions
         public override byte[] PackRequest()
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters p = (ModbusWriteCommandParameters)CommandParameters;
+
+            byte[] request = new byte[12];
+
+            // MBAP
+            request[0] = (byte)(p.TransactionId >> 8);
+            request[1] = (byte)(p.TransactionId & 0xFF);
+            request[2] = (byte)(p.ProtocolId >> 8);
+            request[3] = (byte)(p.ProtocolId & 0xFF);
+            request[4] = (byte)(p.Length >> 8);
+            request[5] = (byte)(p.Length & 0xFF);
+            request[6] = p.UnitId;
+
+            // PDU (FC = 0x06)
+            request[7] = p.FunctionCode;
+            request[8] = (byte)(p.OutputAddress >> 8);
+            request[9] = (byte)(p.OutputAddress & 0xFF);
+            request[10] = (byte)(p.Value >> 8);
+            request[11] = (byte)(p.Value & 0xFF);
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters p = (ModbusWriteCommandParameters)CommandParameters;
+            var result = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            if (response == null || response.Length < 12)
+            {
+                throw new ArgumentException("Invalid Modbus response length.");
+            }
+
+            byte functionCode = response[7];
+            if ((functionCode & 0x80) != 0)
+            {
+                if (response.Length < 9)
+                {
+                    throw new ArgumentException("Invalid Modbus exception response length.");
+                }
+
+                HandeException(response[8]);
+            }
+
+            ushort address = (ushort)((response[8] << 8) | response[9]);
+            ushort echoedValue = (ushort)((response[10] << 8) | response[11]);
+
+            if (address != p.OutputAddress)
+            {
+                throw new ArgumentException("Write register response address mismatch.");
+            }
+
+            result.Add(new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, address), echoedValue);
+            return result;
         }
     }
 }
